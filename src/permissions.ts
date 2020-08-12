d265211d03264f0f22596951de6eb366a860bc9b
Joel Thorstensson <joel@oed.earth>
2020-08-12 15:47:55 +0200

    feat: Impelement permissions module

---
+++ src/permissions.ts
@@ -0,0 +1,84 @@
+import store from 'store'
+
+interface PermissionRequest {
+  type: string
+  origin?: string | null
+  payload: Record<string, any>
+}
+
+export type GetPermissionFn = (req: PermissionRequest) => Promise<Array<string>> | null
+
+export const SELF_ORIGIN = '__IDW_ORIGIN'
+
+const storageKey = (origin: string, did: string) => `3id_permission_${did}_${origin}`
+
+export default class Permissions {
+
+  constructor(protected getPermission: GetPermissionFn) {
+    if (typeof this.getPermission !== 'function') {
+      throw new Error('getPermission parameter has to be a function')
+    }
+  }
+
+  setDID(did: string) {
+    this._did = did
+  }
+
+  /**
+   * Request permission for given paths for a given origin.
+   *
+   * @param     {String}            origin          Application domain
+   * @param     {Array<String>}     paths           The desired paths
+   * @return    {Array<String>}                     The paths that where granted permission for
+   */
+  async request(origin: string, paths: Array<string> = []): Array<string> | null {
+    if (this.has(origin, paths)) {
+      return paths
+    } else {
+      const given = await this.getPermission({
+        type: 'authenticate',
+        origin,
+        payload: { paths }
+      })
+      this.set(origin, given)
+      return given
+    }
+  }
+
+  /**
+   * Determine if permission has been given for paths for a given origin.
+   *
+   * @param     {String}            origin          Application domain
+   * @param     {Array<String>}     paths           The desired paths
+   * @return    {Boolean}                           True if permission has previously been given
+   */
+  has(origin: string, paths: Array<string> = []): boolean {
+    if (origin === SELF_ORIGIN) return true
+    const currentPaths = this.get(origin)
+    return paths.reduce((acc: boolean, path: string) => {
+      return acc && currentPaths.includes(path)
+    }, Boolean(currentPaths))
+  }
+
+  /**
+   * Get the paths which the given origin has permission for.
+   *
+   * @param     {String}            origin          Application domain
+   * @return    {Array<String>}                     The permissioned paths
+   */
+  get(origin: string): Array<string> {
+    if (!this._did) throw new Error('DID not set')
+    return store.get(storageKey(origin, this._did))
+  }
+
+  /**
+   * Set the paths which the given origin should have permission for.
+   *
+   * @param     {String}            origin          Application domain
+   * @param     {Array<String>}     paths           The desired paths
+   */
+  set(origin: string, paths: Array<string> | null): void {
+    if (!this._did) throw new Error('DID not set')
+    return store.set(storageKey(origin, this._did), paths)
+  }
+}
---
+++ test/permissions.test.js
@@ -0,0 +1,57 @@
+import Permissions, { SELF_ORIGIN } from '../src/permissions'
+import store from 'store'
+
+let getPermFn = async req => []
+
+describe('Permissions', () => {
+  let permissions
+  beforeEach(() => {
+    permissions = new Permissions(getPermFn)
+    permissions.setDID('did:3:asdf')
+  })
+
+  afterEach(() => {
+    store.clearAll()
+  })
+
+  it('Correctly constructed', async () => {
+    expect(() => new Permissions()).toThrow(/has to be a function/)
+    permissions = new Permissions(getPermFn)
+    expect(() => permissions.get('app1')).toThrow('DID not set')
+    expect(() => permissions.set('app1')).toThrow('DID not set')
+  })
+
+  it('set / get correctly', async () => {
+    permissions.set('app1', ['/1', '/2'])
+    permissions.set('app2', ['/a', '/b'])
+    expect(permissions.get('app1')).toEqual(['/1', '/2'])
+    expect(permissions.get('app2')).toEqual(['/a', '/b'])
+    permissions.set('app1', null)
+    expect(permissions.get('app1')).toEqual(null)
+  })
+
+  it('has works correctly', async () => {
+    permissions.set('app1', ['/1', '/2'])
+    expect(permissions.has(SELF_ORIGIN)).toBeTruthy()
+    expect(permissions.has(SELF_ORIGIN, ['a', 'b', 'c'])).toBeTruthy()
+    expect(permissions.has('app1')).toBeTruthy()
+    expect(permissions.has('app2')).toBeFalsy()
+    expect(permissions.has('app1', ['/1'])).toBeTruthy()
+    expect(permissions.has('app1', ['/1', '/2'])).toBeTruthy()
+    expect(permissions.has('app1', ['/1', '/2', '/3'])).toBeFalsy()
+  })
+
+  it('request works correctly', async () => {
+    expect(await permissions.request('app1', [])).toEqual([])
+    expect(await permissions.request('app1', ['/1'])).toEqual([])
+    permissions.getPermission = async req => null
+    expect(await permissions.request('app2', [])).toEqual(null)
+    // will remember previously given permissions
+    expect(await permissions.request('app1', [])).toEqual([])
+    permissions.getPermission = async req => ['/1']
+    expect(await permissions.request('app1', ['/1'])).toEqual(['/1'])
+    expect(await permissions.request('app1', ['/1', '/2'])).toEqual(['/1'])
+    permissions.getPermission = async req => ['/1', '/2']
+    expect(await permissions.request('app1', ['/1', '/2'])).toEqual(['/1', '/2'])
+  })
+})
