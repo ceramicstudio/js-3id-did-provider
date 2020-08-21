3b054c0c3ecbebf2e62d1f1b6da33d655b1aafe6
Janko Simonovic <simonovic86@gmail.com>
2020-08-21 13:01:49 +0200

    fix: handle undefined protected

--- src/did-provider.ts
+++ src/did-provider.ts
@@ -48,7 +48,9 @@ export const didMethods: HandlerMethods<Context> = {
     const keyName = threeIdx.parseKeyName(params.did)
     const kid = await threeIdx.encodeKidWithVersion(keyName)
     const signer = keyring.getSigner(keyName)
-    const header = toStableObject(Object.assign(params.protected, { kid }))
+
+    let header = params.protected ? params.protected : {}
+    header = toStableObject(Object.assign(header, { kid }))
     const jws = await createJWS(toStableObject(params.payload), signer, header)
     return { jws }
   },
